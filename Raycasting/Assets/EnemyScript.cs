40210d3d8e17b24582cfd71181a50972c54590e3
Hadházi András <andrashadhazi@gmail.com>
2020-03-19 21:16:56 +0100

    First commit

---
+++ Raycasting/Assets/EnemyScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+    public int health = 100;
+	// Use this for initialization
+	public void damage()
+    {
+        health -= 10;
+        if(health < 0)
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+}
--- Raycasting/Assets/Raycasting.cs
+++ Raycasting/Assets/Raycasting.cs
@@ -8,33 +8,49 @@ public class Raycasting : MonoBehaviour {
    
     public Camera cam;
     public ParticleSystem ps;
+    private float timer = 0f;
+    public int bullets = 10;
+    private EnemyScript EnemyController;
+    
+    
 	// Use this for initialization
 	void Start () {
-		
+        timer = 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0) && timer > 0.25)
         {
             ps.Play();
-            
+            timer = 0;
+            bullets--;
             RaycastHit hit;
             Vector3 rayOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
             if (Physics.Raycast(rayOrigin, cam.transform.forward, out hit, Mathf.Infinity))
             {
                 if (hit.collider.tag == "Enemy")
                 {
-                    Destroy(hit.collider.gameObject);
+                    EnemyController = hit.collider.gameObject.GetComponent<EnemyScript>();
+                    EnemyController.damage();
+                    
                    
                 }
+            }
+            if(bullets == 0)
+            {
+                //újratöltés animáció
+                Debug.Log("reloading");
+                bullets = 10;
             }
                 
                 
         }
-            
-            
-        
-        
+        timer += Time.deltaTime;
+       
+
+
+
+
     }
 }
