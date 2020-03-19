db8d3eb381b120803d2cb9a7f8e499dc9820aeb0
Hadházi András <andrashadhazi@gmail.com>
2020-03-19 21:22:16 +0100

    added gitignore changes

---
+++ Raycasting/Assets/HPController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPController : MonoBehaviour {
+    public Transform Hp;
+    private float health = 300;
+    public float damage = 100f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("k"))
+        {
+            health -= damage;
+            Hp.gameObject.transform.localScale = new Vector3(health, 1, 1);
+            Hp.gameObject.transform.position -= Vector3.right * (damage / 2);
+        }
+	}
+}
